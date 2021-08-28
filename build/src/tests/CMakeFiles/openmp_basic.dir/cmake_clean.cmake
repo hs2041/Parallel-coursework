a90b7ad81d3ad75171f49e9a3f31acd667b0fb0d
hs2041 <hsingh1097@gmail.com>
2021-08-29 02:27:25 +0530

    structured

---
+++ build/src/tests/CMakeFiles/openmp_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openmp_basic.dir/src/test1.cpp.o"
+  "openmp_basic"
+  "openmp_basic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openmp_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hardeep/repos/parallel-course-repo/src/tests
+# Build directory: /home/hardeep/repos/parallel-course-repo/build/src/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
