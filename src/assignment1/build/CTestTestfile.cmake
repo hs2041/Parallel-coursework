f50e55c6afc25290d54521563ad404e3f9a084e3
hs2041 <hsingh1097@gmail.com>
2021-09-05 02:22:29 +0530

    sequential done

---
+++ src/assignment1/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hardeep/repos/parallel-course-repo/src/assignment1
+# Build directory: /home/hardeep/repos/parallel-course-repo/src/assignment1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/assignment1/src/main.cpp
+++ src/assignment1/src/main.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
 #include <omp.h>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <eigen3/Eigen/Dense>
+#include <iomanip>
+#include <chrono>
 
-
-
-#define INPUT_FILENAME "../src/assignment1/input/input.txt"
-#define OUTPUT_FILENAME "../src/assignment1/output/output.txt"
-
-#define $P(X) std::cout<<X<<std::endl;
+#define INPUT_FILENAME "../input/input.txt"
+#define OUTPUT_FILENAME "../output/output_test.txt"
 
 int main(){
+    auto start = std::chrono::high_resolution_clock::now();
 
     std::ifstream infile; 
     infile.open(INPUT_FILENAME);
+
+    std::ofstream outfile;
+    outfile.open(OUTPUT_FILENAME);
 
     int n_testcases;
     int i = 0;
@@ -22,7 +24,7 @@ int main(){
     std::string copystring;
     infile >> copystring;
     n_testcases = std::stoi(copystring);
-    std::cout.precision(10);
+    // std::cout.precision(10);
 
     for(int no_t=0;no_t<n_testcases;no_t++)
     {
@@ -38,10 +40,9 @@ int main(){
         infile >> copystring;
         N = std::stoi(copystring);
 
-        Eigen::MatrixXd data(X+U/2,Y+V/2);
-        Eigen::MatrixXd output_data(X+U-1,Y+V-1);
+        Eigen::MatrixXd data(X+U-1,Y+V-1);
         Eigen::MatrixXd filter(U,V);
-        data = Eigen::MatrixXd::Zero(X+U/2,Y+V/2);
+        data = Eigen::MatrixXd::Zero(X+U-1,Y+V-1);
 
         for(int i=0;i<X;i++)
         {
@@ -59,8 +60,48 @@ int main(){
                 filter(i,j) = std::stod(copystring);
             }
         }
-        std::cout << std::scientific << "Here is the matrix m:\n" << filter(0,2) <<std::endl;
+
+        for (int iter = 0; iter < N; iter++)
+        {
+            Eigen::MatrixXd temp_image(X + U -1, Y + V -1);
+            temp_image = Eigen::MatrixXd::Zero(X + U -1, Y + V -1);
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    double temp_data = 0;
+                    for (int u = 0; u < U; u++)
+                    {
+                        for (int v = 0; v < V;v++)
+                        {
+                            temp_data += (double)data(i + u, j + v) * (double)filter(u, v);
+                        }
+                    }
+                    temp_image(i+U/2,j+V/2)= temp_data;
+                }
+            }
+            data = temp_image;
+        }
+        //simply copying data
+
+        // std::fstream.precision(10);
+        outfile << std::setprecision(10);
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                outfile << std::scientific << data(i + U / 2, j + V / 2) << " ";
+            }
+            outfile << "\n";
+        }
+
     }
-    
+
+    infile.close();
+    outfile.close();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Duration: " << duration.count()<< std::endl;
     return 1;
 }
